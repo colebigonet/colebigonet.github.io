7481de0fd6ea9eae26c4944ddd872192986271d5
Cole <Cole@Donna-Lomps-Computer.local>
2016-05-17 13:42:56 -0400

    Updated little cello visualization so that sound hole changes color.

--- work/celloviz.js
+++ work/celloviz.js
@@ -8,7 +8,7 @@ canvas.height = HEIGHT;
 var audio = new Audio();
 //audio.crossOrigin = "anonymous";
 audio.src = 'audio/Little Cello - Cole Bigonet.mp3';
-audio.controls = true;
+audio.controls = false;
 audio.autoplay = true;
 document.body.appendChild(audio);
 
@@ -21,19 +21,26 @@ window.addEventListener('load', function(e) {
   var source = context.createMediaElementSource(audio);
   source.connect(analyser);
   analyser.fftSize = 2048;
-  drawContext.beginPath();
+  /*drawContext.beginPath();
   drawContext.arc(WIDTH/2, HEIGHT/2, 50, 0, 2 * Math.PI, false);
-  drawContext.fill();
+  drawContext.fill();*/
 
   var sample = function(timestamp) {
     var bufferLength = analyser.frequencyBinCount;
     var dataArray = new Uint8Array(bufferLength);
     analyser.getByteTimeDomainData(dataArray);
     drawContext.clearRect(0, 0, WIDTH, HEIGHT);
-    drawContext.beginPath();
+    /*drawContext.beginPath();
   drawContext.arc(WIDTH/2, HEIGHT/2-10, 100, 0, 2 * Math.PI, false);
   drawContext.fillStyle = '#eeeeee';
-        drawContext.fill();
+        drawContext.fill();*/
+      drawContext.beginPath();
+        drawContext.arc(WIDTH/2, HEIGHT/2-10, 100, 0, 2 * Math.PI, false);
+        //drawContext.fillStyle = '#c6c6c6';
+        var b = Math.floor(255*Math.random());
+        //console.log(b);
+        drawContext.fillStyle = "rgba(232,166," + b + ",1)";
+        drawContext.fill();    
     //test code:
     for(var string=0; string<4; string++){
       for (var i = 0; i < analyser.frequencyBinCount; i++) {
@@ -47,7 +54,10 @@ window.addEventListener('load', function(e) {
 
         /*drawContext.beginPath();
         drawContext.arc(WIDTH/2, HEIGHT/2-10, 100, 0, 2 * Math.PI, false);
-        drawContext.fillStyle = '#c6c6c6';
+        //drawContext.fillStyle = '#c6c6c6';
+        var b = Math.floor(255*Math.random());
+        //console.log(b);
+        drawContext.fillStyle = "rgba(232,166," + b + ",0.2)";
         drawContext.fill();*/
       }
     }  
